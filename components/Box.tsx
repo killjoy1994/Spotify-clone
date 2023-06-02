502313518e917084e7cee817808cd04cd0acfb42
Bagus Nugroho <papaobobo@gmail.com>
2023-06-02 23:38:55 +0700

    create box component

---
+++ components/Box.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { twMerge } from "tailwind-merge";
+
+interface BoxProps {
+  children: React.ReactNode;
+  classname?: string;
+}
+
+const Box: React.FC<BoxProps> = ({ children, classname }) => {
+  return <div className={twMerge(`bg-neutral-900 rounder-lg h-fit w-full`, classname)}>{children}</div>;
+};
+
+export default Box;
